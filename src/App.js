7e31e79549a4bcb36440e27918d76ebca3633899
liulvjian <634098330@qq.com>
2017-07-18 09:44:31 +0800

    use css module

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import Category from '_/components/category'
 import Book from '_/components/book'
 
 import '_/styles/index.scss'
+import _header from '_/styles/header.scss'
+import _menu from '_/styles/menu.scss'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <div>
+          <div className={_header.container}>
             <Link to="/author">作者</Link>
             <Link to="/category">分类</Link>
             <Link to="/book">书籍</Link>
           </div>
 
-          <div>
+          <div className={_menu.container}>
             <Route path="/author" component={Author} />
             <Route path="/category" component={Category} />
             <Route path="/book" component={Book} />
